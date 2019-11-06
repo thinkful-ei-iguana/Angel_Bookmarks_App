1fb93bf7ff46590008db2b4bb2c21890381fc3c0
Angel Alicea <ala31996@gmail.com>
2019-11-06 11:34:51 -0500

    added more empty functions to apis.js

--- src/scripts/api.js
+++ src/scripts/api.js
@@ -1,8 +1,16 @@
+import $ from 'jquery';
 
 const BASE_URL = 'https://thinkful-list-api.herokuapp.com/Angel/bookmarks';
 
+const getBookmarks = function () {};
+const addBookmark = function () {};
+const updateBookmarks = function () {};
+const deleteBookmark = function () {};
 
 
 export default {
-  
+  getBookmarks,
+  addBookmark,
+  updateBookmarks,
+  deleteBookmark
 }
